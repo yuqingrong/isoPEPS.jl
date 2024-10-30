ef50be89b54bcc6a2b928b8e212802dc19469ba5
Documenter.jl <documenter@juliadocs.github.io>
2024-10-30 10:30:59 +0000

    build based on 83fc412

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = isoPEPS","category":"page"},{"location":"#isoPEPS","page":"Home","title":"isoPEPS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for isoPEPS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [isoPEPS]","category":"page"}]
+}
